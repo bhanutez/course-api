b459e05f192194f6cbe3d3043b17c075090a8f9e
Bhanu Teja <bhanutez.techie@gmail.com>
2020-02-13 14:39:53 +0530

    Add put api to update topic

--- src/main/java/com/tw/courseapi/topics/TopicController.java
+++ src/main/java/com/tw/courseapi/topics/TopicController.java
@@ -1,9 +1,7 @@
 package com.tw.courseapi.topics;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +20,15 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.update(id, topic);
     }
 }
--- src/main/java/com/tw/courseapi/topics/TopicService.java
+++ src/main/java/com/tw/courseapi/topics/TopicService.java
@@ -2,16 +2,17 @@ package com.tw.courseapi.topics;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "spring framework", "spring framework description"),
             new Topic("java", "core java", "core java description"),
             new Topic("javascript", "javascript", "javascript description")
-            );
+    ));
 
     public List<Topic> getTopics() {
         return topics;
@@ -19,5 +20,17 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(element -> element.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void update(String id, Topic topic) {
+        for(int index = 0; index < topics.size(); index++) {
+            if(topics.get(index).getId().equals((id))) {
+                topics.set(index, topic); return;
+            }
+        }
     }
 }
